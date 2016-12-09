ccf582e582d37294cb64c93c0eeda52e4b0cc09a
cfatrane <cfatrane@student.42.fr>
2016-12-09 14:59:12 +0100

    fdf

--- ft_draw_map.c
+++ ft_draw_map.c
@@ -6,7 +6,7 @@
 /*   By: cfatrane <cfatrane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/05 18:32:44 by cfatrane          #+#    #+#             */
-/*   Updated: 2016/12/08 19:24:13 by cfatrane         ###   ########.fr       */
+/*   Updated: 2016/12/09 14:57:54 by cfatrane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,40 +79,49 @@ int	ft_draw_parall_col(int **map, int nblin, int nbcol, void *mlx, void *win)
 	int x;
 	int y;
 	int c;
-	int abs;
-	int ord;
-	double cte = 1;
-	int k = 20;
+	int col;
+	int line;
+	double cte = 5;
+	int k = 50;
 
+	line = 0;
 	y = 0;
-	abs = 0;
-	ord = 0;
 	while (y < (nblin - 1) * 20)
 	{
-		abs = 0;
+		printf("LIGNE = %d\n\n", y);
+		col = 0;
 		x = 0;
-		X1 = x + cte * (k * map[ord][abs]);
-		Y1 = y + (cte / 2) * (k * map[ord][abs]);
+		X1 = x + cte * (15 * map[line][col]);
+		Y1 = y + (cte / 2) *  (15 * map[line][col]);
 		while (x < nbcol * 20)
 		{
-			X2 = x + cte * (k * map[ord + 1][abs]);
-			Y2 = (y + 20) + (cte / 2) * (k * map[ord + 1][abs]);
+			X2 = (x + 20) + cte * (15 * map[line + 1][col]);
+			Y2 = (y + 20) + (cte / 2) * (15 * map[line + 1][col]);
+			printf("X1 vaut = %d\n", X1);
+			printf("Y1 vaut = %d\n", Y1);
+			printf("X2 vaut = %d\n", X2);
+			printf("Y2 vaut = %d\n", Y2);
 			c = Y1;
 			while (c <= Y2)
 			{
-				mlx_pixel_put(mlx, win, 50 + ((X1 + ((X2 - X1) * (c - Y1)) / (Y2 - Y1))), 50 + c, 0x00FFFFFF);
+				mlx_pixel_put(mlx, win, 50 + (X1 + ((X2 - X1) * (c - Y1)) / (Y2 - Y1)), 50 + c, 0x000000FF);
+				mlx_pixel_put(mlx, win, k + X1, k + Y1, 0x00FF0000);
 				c++;
 			}
-			abs++;
-			X1 = X1 + 20;
+			printf("--------------\n");
+			col++;
 			x = x + 20;
+			X1 = X2;
+			Y1 = Y2;
 		}
-		ord++;
+		printf("--------------------------\n");
+		printf("NEW LINE NEW LINE NEW LINE\n");
+		printf("--------------------------\n");
+		line++;
 		y = y + 20;
 	}
 	return (0);
 }
-
 
 int	ft_draw_parall_line(int **map, int nblin, int nbcol, void *mlx, void *win)
 {
@@ -123,35 +132,44 @@ int	ft_draw_parall_line(int **map, int nblin, int nbcol, void *mlx, void *win)
 	int x;
 	int y;
 	int c;
-	int abs;
-	int ord;
-	double cte = 1;
-	int k = 20;
+	int col;
+	int line;
+	double cte = 5;
+	int k = 50;
 
-	ord = 0;
+	line = 0;
 	y = 0;
 	while (y < nblin * 20) // OK
 	{
-		abs = 0;
+//		printf("LIGNE = %d\n\n", y);
+		col = 0;
 		x = 0;
-		Y1 = y + (cte / 2) * (k * map[ord][abs]);
-		X1 = x + cte * (k * map[ord][abs]);
+		X1 = x + cte * (15 * map[line][col]);
+		Y1 = y + ((cte / 2) * (15 * map[line][col]));
 		while (x < nbcol * 20) // OK
 		{
-			X2 = (x + 20) + cte * (k * map[ord][abs + 1]);
-			Y2 = y + (cte / 2) * (k * map[ord][abs + 1]);
+			X2 = (x + 20) + cte * (15 * map[line][col + 1]);
+			Y2 = y + (cte / 2) * (15 * map[line][col + 1]);
+/*			printf("X1 vaut = %d\n", X1);
+			printf("Y1 vaut = %d\n", Y1);
+			printf("X2 vaut = %d\n", X2);
+			printf("Y2 vaut = %d\n", Y2);*/
 			c = X1;
 			while (c <= X2)
 			{
-				mlx_pixel_put(mlx, win, 50 + c, 50 + ((Y1 + ((Y2 - Y1) * (c - X1)) / (X2 - X1))), 0x0000FFFF);
+				mlx_pixel_put(mlx, win, 50 + c, 50 + (Y1 + (ft_abs(Y2 - Y1) * (c - X1)) / ft_abs(X2 - X1)), 0x00FF0000);
 				c++;
 			}
-			abs++;
-			X1 = X1 + 20;
+//			printf("--------------\n");
+			col++;
 			x = x + 20;
+			X1 = X2;
+			Y1 = Y2;
 		}
-		ord++;
-	//	Y1 = Y1 + 20;
+/*		printf("--------------------------\n");
+		printf("NEW LINE NEW LINE NEW LINE\n");
+		printf("--------------------------\n");*/
+		line++;
 		y = y + 20; // OK
 	}
 	return (0);
@@ -164,7 +182,8 @@ int	ft_draw_map(int **map, int nblin, int nbcol)
 
 	(void)map;
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, 1000, 1000, "fdf");
+	win = mlx_new_window(mlx, 1500, 1500, "fdf");
+//	ft_putnbr(map[8][6]);
 //	ft_draw_line(map, nblin, nbcol, mlx, win);
 //	ft_draw_colonne(map, nblin, nbcol, mlx, win);
 	ft_draw_parall_line(map, nblin, nbcol, mlx, win);
